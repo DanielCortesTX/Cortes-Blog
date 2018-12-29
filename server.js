80ddd0665be0309d1062db907ed5ef0f8d8f5d6a
Daniel Cortes <d5t5c5@yahoo.com>
2018-12-28 20:50:47 -0600

    second deploy to heroku

--- server.js
+++ server.js
@@ -31,11 +31,13 @@ require('./config/passport')(passport)
 app.use('/api/users', users)
 app.use('/api/posts', posts)
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
 
-app.get('/', (req, res) => res.send('Hello world'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000
 
