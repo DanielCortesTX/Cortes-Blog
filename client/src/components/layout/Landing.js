1f222a130d984beea1495f32d4a981065bed3d00
Daniel Cortes <d5t5c5@yahoo.com>
2019-01-07 12:51:27 -0600

    fix: added polyfill. Testing other browsers

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -10,7 +10,7 @@ class Landing extends Component {
             <div>
               <div className="text-center display-3">
                 <h1 className="display-3 lead small">Cortes Blog</h1>
-                <p className="lead">The musings and introspection of Daniel Cortes</p>
+                <p className="lead">The musings and introspection of a one Daniel Cortes</p>
                 <Link to="/register" className="btn btn-lg btn-primary mr-2">Sign up</Link>
                 <Link to="/login" className="btn btn-lg btn-primary">Login</Link>
               </div>
