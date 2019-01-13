9c8f988c5e26b598a13d9b313cdf63030f75b0d4
Daniel Cortes <d5t5c5@yahoo.com>
2019-01-12 20:28:04 -0600

    fix: adjusted spacing of home feed
    Added classnames with a conditional for when too few blog posts exist
    in database

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import BlogLink from './BlogLink'
 import { getPosts } from '../../actions/postActions'
+import classnames from 'classnames'
 
 // @desc Inherits loading property until posts are returned from store. Posts are mapped through and given BlogLink component to display information.
 
@@ -20,7 +21,7 @@ class Home extends Component {
     }
 
     return (
-      <div className="container p-4 load-adjust">
+      <div className={classnames('container, p-4', { 'load-adjust': posts.length < 3})}>
         <h1 className="text-center text-uppercase lead display-4 mt-4 mb-4 pb-4">Cortes Corner</h1>
           {postFeed}
       </div>
