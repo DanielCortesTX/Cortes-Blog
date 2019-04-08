e04ed3054dd7b3d3c2934749ff44c77f152ea8b3
Daniel Cortes <d5t5c5@yahoo.com>
2019-04-07 23:26:26 -0500

    feat: Adjusted sizing for single post page.

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default () => {
   return (
-    <footer className="bg-secondary text-white navbar-dark p-4 text-center">Site created and maintained by Daniel Cortes</footer>
+    <footer className="bg-secondary text-white mt-5 navbar-dark p-4 text-center">Site created and maintained by Daniel Cortes</footer>
   )
 }
