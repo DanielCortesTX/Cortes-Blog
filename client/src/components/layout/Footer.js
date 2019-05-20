47140de60baddec159879d6fdd712aba328491a9
Daniel Cortes <d5t5c5@yahoo.com>
2019-05-20 12:21:57 -0500

    feat: Created new styling directories
    - Made component folder for component specific modifiers
    - Made small folder for simple specific modifications
    - Made spacing folder for modifiers that are spacing specific
    - Moved modifies to relevant folders and files

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default () => {
   return (
-    <footer className="bg-secondary sass-test navbar-dark p-4 text-center">Site created and maintained by Daniel Cortes</footer>
+    <footer className="bg-secondary navbar-dark p-4 text-center">Site created and maintained by Daniel Cortes</footer>
   )
 }
