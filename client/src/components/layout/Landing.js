73304da1e0da53fc2ff031b5e33f657e67617cf2
unknown <dcorteswebdev@gmail.com>
2020-08-11 08:53:51 -0500

    feat: Cleaned Landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -4,19 +4,15 @@ import { Link } from 'react-router-dom'
 class Landing extends Component {
   render() {
     return (
-      <div className="landing">
-        <div className="landing-inner text-light">
-          <div className="container">
-            <div>
-              <div className="text-center display-3">
-                <h1 className="display-3 lead small">Cortes Blog</h1>
-                <p className="lead">The musings and introspection of a one Daniel Cortes</p>
-                <Link to="/register" className="btn btn-lg btn-primary mr-2">Sign up</Link>
-                <Link to="/login" className="btn btn-lg btn-primary">Login</Link>
-              </div>
-            </div>
+      <div className="landing text-light">
+        <div className="container">
+          <div className="text-center">
+            <h1 className="display-3 lead">Cortes Blog</h1>
+            <p className="lead">The musings and introspection of a one Daniel Cortes</p>
+            <Link to="/register" className="btn btn-lg btn-primary mr-2">Sign up</Link>
+            <Link to="/login" className="btn btn-lg btn-primary">Login</Link>      
           </div>
-        </div>
+          </div>         
       </div>
     )
   }
